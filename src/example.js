57f013327fada0e9648daac3d725ca023087141d
deepakkjfrog <deepakk@jfrog.com>
2025-07-31 16:09:56 +0530

    N8N commit

--- src/example.js
+++ src/example.js
@@ -1,13 +1,1 @@
-// This is a new line added by automated script: 2025-07-30 17:07:05
-// This is a new line added by automated script: 2025-07-31 10:20:21
-
-// This is a new line added by automated script: " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "
-   
-Some new line are added here
-One more
-
-def add_two_numbers(num1, num2):
-    return num1 + num2
-
-def add_two_numbers(num1, num2):
-    return num1 + num2
+This message is being pushed by n8n workflow
